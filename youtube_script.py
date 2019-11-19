e78d40c01eedc69360f47ca3a7522f02592ef18c
laddi <meladbrook@gmail.com>
2019-11-18 19:39:15 -0800

    Completed youtube_script.py

--- youtube_script.py
+++ youtube_script.py
@@ -8,25 +8,37 @@ import requests
 with open("api_creds.json", "r") as read_file:
     data = json.load(read_file)
 
-api_url = "https://www.googleapis.com/youtube/v3/search?" \
-          "channelId=UCJFp8uSYCjXOMnkUyb3CQ3Q&part=snippet" \
-          "&key={}".format(data["api"])
+channel_id ="UCJFp8uSYCjXOMnkUyb3CQ3Q"
 
-api_response = requests.get(api_url)
-videos = json.loads(api_response.text)
 
-with open("youtube_videos.csv", "w") as csv_file:
-    csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(["publishedAt",
-                         "title",
-                         "description",
-                         "thumbnailurl"])
-    if videos.get("items") is not None:
-        for video in videos.get("items"):
-            video_data_row = [
-                video["snippet"]["publishedAt"],
-                video["snippet"]["title"],
-                video["snippet"]["description"],
-                video["snippet"]["thumbnails"]["default"]["url"]
-                ]
-            csv_writer.writerow(video_data_row)
+def make_csv(page_id):
+    base = "https://www.googleapis.com/youtube/v3/search?"
+    fields = "&part=snippet&channelId="
+    api_key = "&key={}".format(data["api"])
+    api_url = base + fields + channel_id + api_key
+    api_response = requests.get(api_url)
+    videos = json.loads(api_response.text)
+    with open("%syoutube_videos.csv" % page_id, "w") as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(["publishedAt",
+                             "title",
+                             "description",
+                             "thumbnailurl"])
+        has_another_page = True
+        while has_another_page:
+            if videos.get("items") is not None:
+                for video in videos.get("items"):
+                    video_data_row = [
+                        video["snippet"]["publishedAt"],
+                        video["snippet"]["title"],
+                        video["snippet"]["description"],
+                        video["snippet"]["thumbnails"]["default"]["url"]
+                        ]
+                    csv_writer.writerow(video_data_row)
+            if "nextPageToken" in videos.keys():
+                next_page_url = api_url + "&pageToken="+videos["nextPageToken"]
+                next_page_posts = requests.get(next_page_url)
+                videos = json.loads(next_page_posts.text)
+            else:
+                print("no more videos!")
+                has_another_page = False
